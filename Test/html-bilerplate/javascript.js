a2765d55e5b7a022cf529de064095340fdc89e59
Rujul Dwivedi <rujuldwivedi@icloud.com>
2025-07-31 19:24:33 +0530

    Finished basics

---
+++ Test/html-bilerplate/javascript.js
@@ -0,0 +1,76 @@
+console.log("Hello, World!");
+
+let firstname = "Rujul";
+let surname = "Dwivedi";
+
+console.log(firstname);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+const pi = 3.14;
+
+// pi = 10;
+
+console.log(pi);
+
+console.log(3 + 2 - 76 * (1 + 1));
+
+let a = 10;
+
+console.log(a);
+
+a = "Hello";
+
+console.log(a);
+
+function func(animal) {
+  return animal + " is my favorite animal!";
+}
+
+console.log(func("Goat"));
+
+const prompt = require("prompt-sync")();
+// Very important
+
+let answer = parseInt(prompt("Enter the number"));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
+}
+
+const myObject = {
+  property: "Value!",
+  otherProperty: 77,
+  "obnoxious property": function () {
+    // do stuff
+  },
+};
+
+console.log(myObject.property);
+
+const variable = "property";
+
+console.log(myObject[variable]);
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+
+  this.sayName = function () {
+    console.log(this.name);
+  };
+}
+
+const player = new Player("Rujul", "X");
+console.log(player.name);
+
+player.sayName();
