260e84e6deb3bcdbf69463bb66481eaf4c168e6a
Rujul Dwivedi <rujuldwivedi@icloud.com>
2025-08-01 14:56:06 +0530

    Updated files

--- Test/html-bilerplate/javascript.js
+++ Test/html-bilerplate/javascript.js
@@ -1,76 +1,86 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
 
-let firstname = "Rujul";
-let surname = "Dwivedi";
+// let firstname = "Rujul";
+// let surname = "Dwivedi";
 
-console.log(firstname);
-console.log(surname);
+// console.log(firstname);
+// console.log(surname);
 
-let age = 11;
-console.log(age);
+// let age = 11;
+// console.log(age);
 
-age = 54;
+// age = 54;
 
-console.log(age);
+// console.log(age);
 
-const pi = 3.14;
+// const pi = 3.14;
 
-// pi = 10;
+// // pi = 10;
 
-console.log(pi);
+// console.log(pi);
 
-console.log(3 + 2 - 76 * (1 + 1));
+// console.log(3 + 2 - 76 * (1 + 1));
 
-let a = 10;
+// let a = 10;
 
-console.log(a);
+// console.log(a);
 
-a = "Hello";
+// a = "Hello";
 
-console.log(a);
+// console.log(a);
 
-function func(animal) {
-  return animal + " is my favorite animal!";
-}
+// function func(animal) {
+//   return animal + " is my favorite animal!";
+// }
 
-console.log(func("Goat"));
+// console.log(func("Goat"));
 
-const prompt = require("prompt-sync")();
-// Very important
+// const prompt = require("prompt-sync")();
+// // Very important
 
-let answer = parseInt(prompt("Enter the number"));
+// let answer = parseInt(prompt("Enter the number"));
 
-for (let i = 1; i <= answer; i++) {
-  if (i % 15 == 0) console.log("FizzBuzz");
-  else if (i % 3 == 0) console.log("Fizz");
-  else if (i % 5 == 0) console.log("Buzz");
-  else console.log(i);
-}
+// for (let i = 1; i <= answer; i++) {
+//   if (i % 15 == 0) console.log("FizzBuzz");
+//   else if (i % 3 == 0) console.log("Fizz");
+//   else if (i % 5 == 0) console.log("Buzz");
+//   else console.log(i);
+// }
 
-const myObject = {
-  property: "Value!",
-  otherProperty: 77,
-  "obnoxious property": function () {
-    // do stuff
-  },
-};
+// const myObject = {
+//   property: "Value!",
+//   otherProperty: 77,
+//   "obnoxious property": function () {
+//     // do stuff
+//   },
+// };
 
-console.log(myObject.property);
+// console.log(myObject.property);
 
-const variable = "property";
+// const variable = "property";
 
-console.log(myObject[variable]);
+// console.log(myObject[variable]);
 
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
+// function Player(name, marker) {
+//   this.name = name;
+//   this.marker = marker;
 
-  this.sayName = function () {
-    console.log(this.name);
-  };
-}
+//   this.sayName = function () {
+//     console.log(this.name);
+//   };
+// }
 
-const player = new Player("Rujul", "X");
-console.log(player.name);
+// const player = new Player("Rujul", "X");
+// console.log(player.name);
 
-player.sayName();
+// player.sayName();
+
+// let msg = document.getElementById("message");
+// console.log(msg);
+
+// let btn = document.getElementsByName("language");
+// console.log(btn);
+
+// let heading = document.getElementsByTagName("h1");
+// console.log(heading);/
+
